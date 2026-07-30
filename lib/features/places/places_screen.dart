704000e2956751d8794b2111f735b48161c1bb19
Sargius <Dr.Sargius@gmail.com>
2026-07-30 09:45:24 +0400

    - fix: small cleanup [skip ci]

--- lib/features/places/places_screen.dart
+++ lib/features/places/places_screen.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/material.dart';
 import '../../core/localization/localized_strings.dart';
-import '../../l10n/app_localizations.dart';
+// import '../../l10n/app_localizations.dart';
 
 class PlacesScreen extends StatelessWidget {
   const PlacesScreen({super.key, required this.categoryName});
@@ -11,7 +11,7 @@ class PlacesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final l10n = AppLocalizations.of(context);
+    // final l10n = AppLocalizations.of(context);
     final l10nStrings = context.strings;
 
     final mockPlaces = List.generate(10, (index) => '$categoryName Place ${index + 1}',);
